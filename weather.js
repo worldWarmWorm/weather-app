0f619da6fb95f853cfb09199e800f0e61c31a770
worldwarmworm <st.valeriq@gmail.com>
2022-01-26 22:41:40 +0700

    created log and storage services

--- weather.js
+++ weather.js
@@ -1,18 +1,27 @@
 #!/usr/bin/env node
 import { getArgs } from "./helpers/args.js";
+import { printHelp, printError, printSuccess } from "./services/log.service.js";
+import { saveKeyValue } from "./services/storage.service.js";
+
+const saveToken = async (token) => {
+  try {
+    await saveKeyValue("token", token);
+    printSuccess("Токен сохранен");
+  } catch (error) {
+    printError(error.message);
+  }
+};
 
 const initCLI = () => {
   const args = getArgs(process.argv);
-  console.log(args);
   if (args.h) {
-    // output help
+    printHelp();
   } else if (args.s) {
-    // save city
   } else if (args.t) {
-    // save token
+    return saveToken(args.t);
   } else {
     // output weather
   }
-}
+};
 
 initCLI();
