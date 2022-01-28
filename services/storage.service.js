380e0534d01ecab42c64ea671e78066532395fc6
worldwarmworm <st.valeriq@gmail.com>
2022-01-28 20:46:57 +0700

    refactor storage service, add api service with the method getWeather

--- services/storage.service.js
+++ services/storage.service.js
@@ -3,6 +3,10 @@ import { join } from "path";
 import { promises } from "fs";
 
 const filePath = join(homedir(), "weather-data.json");
+const TOKEN_DICTIONARY = {
+  token: "token",
+  city: "city"
+} 
 
 const saveKeyValue = async (key, value) => {
   let data = {};
@@ -35,4 +39,4 @@ const isExist = async (path) => {
   }
 };
 
-export { saveKeyValue, getKeyValue };
+export { saveKeyValue, getKeyValue, TOKEN_DICTIONARY };
--- weather.js
+++ weather.js
@@ -1,11 +1,16 @@
 #!/usr/bin/env node
 import { getArgs } from "./helpers/args.js";
+import { getWeather } from "./services/api.service.js";
 import { printHelp, printError, printSuccess } from "./services/log.service.js";
-import { saveKeyValue } from "./services/storage.service.js";
+import { saveKeyValue, TOKEN_DICTIONARY } from "./services/storage.service.js";
 
 const saveToken = async (token) => {
+  if (!token.length) {
+    printError("Не передан токен");
+    return;
+  }
   try {
-    await saveKeyValue("token", token);
+    await saveKeyValue(TOKEN_DICTIONARY.token, token);
     printSuccess("Токен сохранен");
   } catch (error) {
     printError(error.message);
@@ -19,9 +24,8 @@ const initCLI = () => {
   } else if (args.s) {
   } else if (args.t) {
     return saveToken(args.t);
-  } else {
-    // output weather
   }
+  getWeather("novosibirsk");
 };
 
 initCLI();
