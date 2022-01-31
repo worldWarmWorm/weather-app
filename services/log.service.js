93c7fc2ffdbbe63aadd82962cdea987da1fb029c
worldwarmworm <st.valeriq@gmail.com>
2022-01-31 13:39:39 +0700

    formated output

--- services/log.service.js
+++ services/log.service.js
@@ -1,6 +1,21 @@
 import chalk from "chalk";
 import dedent from "dedent-js";
 
+const _getPrettyDate = date => {
+  return new Intl.DateTimeFormat('ru-RU', {
+    day: '2-digit',
+    month: 'long',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit'
+  }).format(new Date(date * 1000))
+}
+
+const _capitalizeFirstLetter = str => {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
 const printError = (error) => {
   console.log(`${chalk.bgRed(" ERROR! ")} ${error}`);
 };
@@ -20,12 +35,13 @@ const printHelp = () => {
 };
 
 const printWeather = (res) => {
+  console.log(res)
   console.log(dedent`
-    ${chalk.bgYellow(" WEATHER ")} Погода в городе ${res.name}
-    ${res.weather[0].description}
-    Температура: ${res.main.temp}, ощущается как ${res.main.feels_like}
+    ${chalk.bgMagenta(" WEATHER ")} Погода в городе ${res.name} по состоянию на ${_getPrettyDate(res.dt)}:
+    ${_capitalizeFirstLetter(res.weather[0].description)}
+    Температура: ${res.main.temp} C°, ощущается как ${res.main.feels_like} C°
     Влажность: ${res.main.humidity}%
-    Скорость ветра: ${res.wind.speed}
+    Скорость ветра: ${res.wind.speed} м/с
   `);
 };
 
